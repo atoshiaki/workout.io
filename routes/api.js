84c2c6c8399f907c8c863ce2c54bcbd33898a4e6
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-12-04 06:34:00 -0800

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const exercise = require("../model/workout.js");
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+      .then(workout => {
+        res.json(workout);
+      }) 
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+router.put('/api/workouts/:id', ( req, res) => {
+    Workout.findById(req.params.id)
+        .exec()
+        .then(workout => {
+            workout.exercises.push(req.body);
+            workout.save()
+                .then(workout => {
+                    res.json(workout);
+                })
+                .catch(err => {
+                    res.status(400).json(err);
+                });
+        })
+        .catch(err => {
+            res.status(400).json(err);
+          });
+});
+
+router.get('/api/workouts/', ( req ,res) => {
+    Workout.find({})
+        .sort([['day', -1]])
+        .limit(1)
+        .exec()
+        .then(workout => {
+            res.json(workout);
+          }) 
+          .catch(err => {
+            res.status(400).json(err);
+          });
+
+});
+
+router.get('/api/workouts/range', ( req ,res) => {
+    Workout.find({})
+        .sort([['day', -1]])
+        .exec()
+        .then(workouts => {
+            console.log(workouts)
+            res.json(workouts);
+          }) 
+          .catch(err => {
+            res.status(400).json(err);
+          });
+
+});
+
+module.exports = router;
