f823c290647d83807bcb04769ec83eb17956c52b
atoshiaki <66647837+atoshiaki@users.noreply.github.com>
2020-12-04 08:47:46 -0800

    Add files via upload

--- model/workout.js
+++ model/workout.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
+const workout = new Schema({
     day: {
         type: Date,
         default: Date.now
@@ -15,10 +15,10 @@ const workoutSchema = new Schema({
         } 
     });
 
-workoutSchema.virtual('totalDuration').get(function() {
+workout.virtual('totalDuration').get(function() {
     return this.exercises.reduce((total, exercise)=> {
         return total + exercise.duration;
     }, 0);      
 }); 
-const Workout = mongoose.model('Workout', workoutSchema);
+const Workout = mongoose.model('Workout', workout);
 module.exports = Workout;
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,6 @@
 const router = require("express").Router();
 const Workout = require("../model/workout.js");
-router.get('/api/workouts/', ( req ,res) => {
-    Workout.find({})
-        .sort([['day', -1]])
-        .limit(1)
-        .exec()
-        .then(workout => {
-            res.json(workout);
-          }) 
-          .catch(err => {
-            res.status(400).json(err);
-          });
 
-});
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
       .then(workout => {
@@ -39,7 +27,19 @@ router.put('/api/workouts/:id', ( req, res) => {
             res.status(400).json(err);
           });
 });
+router.get('/api/workouts/', ( req ,res) => {
+    Workout.find({})
+        .sort([['day', -1]])
+        .limit(1)
+        .exec()
+        .then(workout => {
+            res.json(workout);
+          }) 
+          .catch(err => {
+            res.status(400).json(err);
+          });
 
+});
 router.get('/api/workouts/range', ( req ,res) => {
     Workout.find({})
         .sort([['day', -1]])
@@ -53,5 +53,4 @@ router.get('/api/workouts/range', ( req ,res) => {
           });
 
 });
-
 module.exports = router;
